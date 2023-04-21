0405b8634350ab3feefa415abc1edba4099b04c1
Trusov <a.trusov@voskhod.ru>
2023-04-22 01:24:37 +0400

    Client Sample (google)

---
+++ src/main/java/com/papont/http/socket/SocketRunner.java
@@ -0,0 +1,25 @@
+package com.papont.http.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SocketRunner {
+
+    public static void main(String[] args) throws IOException {
+        // http  - 80
+        // https - 443
+        // tcp
+        InetAddress inetAddress = Inet4Address.getByName("google.com");
+        try (var socket = new Socket(inetAddress, 80);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream())) {
+            outputStream.writeUTF("Hello");
+            var response = inputStream.readAllBytes();
+            System.out.println(response.length);
+        };
+    }
+}
