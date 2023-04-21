2c34d48cea68a66d7a290931f03c88f75e840599
Trusov <a.trusov@voskhod.ru>
2023-04-22 01:24:50 +0400

    Server sample

---
+++ src/main/java/com/papont/http/socket/SocketServerRunner.java
@@ -0,0 +1,21 @@
+package com.papont.http.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class SocketServerRunner {
+
+    public static void main(String[] args) throws IOException {
+        try (var serverSocket = new ServerSocket(7777);
+             var socket = serverSocket.accept();
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream())){
+
+            System.out.println("Client request: " + inputStream.readUTF());
+            outputStream.writeUTF("Hello from server");
+
+        }
+    }
+}
